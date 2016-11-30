531336550ca30dcd5a5cc010570109731ba082be
sxnwlfkk <saxonwolfkok@gmail.com>
2016-11-30 07:02:50 +0100

    Basic pebble clay setup
    The test config page on phone works

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,3 +1,10 @@
+
+// Clay support //
+var Clay = require('pebble-clay');
+var clayConfig = require('./config.json');
+var clay = new Clay(clayConfig);
+
+
 Pebble.addEventListener('ready', function() {
   // PebbleKit JS is ready!
   console.log('PebbleKit JS ready!');
