087e026290ca4f936ea2e957b77967072964cc97
sxnwlfkk <saxonwolfkok@gmail.com>
2016-12-06 06:44:07 +0100

    Added test dictionary to index.js, and sending code

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -4,6 +4,16 @@ var Clay = require('pebble-clay');
 var clayConfig = require('./config.json');
 var clay = new Clay(clayConfig);
 
+var routine_dict = {
+  'Operation': 1,
+  'Routine_Id': 0,
+  'Routine_Name': "Test Routine",
+  'Routine_Item_No': 5,
+  'Wakeup_On_Start': 0,
+  'Item_Names': ["First", "Second", "Third", "Fourth", "Fifth"],
+  'Item_Times': [60, 60, 60, 60, 60]
+}
+
 
 Pebble.addEventListener('ready', function() {
   // PebbleKit JS is ready!
@@ -11,4 +21,7 @@ Pebble.addEventListener('ready', function() {
 
   // Update s_js_ready on watch
   Pebble.sendAppMessage({'JSReady': 1});
+  // Sending test dict
+  Pebble.sendAppMessage(routine_dict);
+  console.log('Sending test dictionary.');
 });
